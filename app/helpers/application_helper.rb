34e6aefc3034f6c160909fc67ec165fd1a61bcd1
Rui Freitas <dinnerpartysaboteur@gmail.com>
2018-11-22 08:37:34 +0200

    base app for lecture done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def set_stars(rating)
+    result = ""
+    for i in 1..5
+      if i > rating
+        result << '<span class="star-icon">☆</span>'
+      else
+        result << '<span class="star-icon full">☆</span>'
+      end
+    end
+    return result.html_safe
+  end
 end
